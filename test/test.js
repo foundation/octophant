05771a903bb328c4bdbd53d584b7e0c3da2477f3
Geoff Kimball <geoff@zurb.com>
2015-08-20 22:47:13 -0700

    Separate variable formatting into its own function

--- test/test.js
+++ test/test.js
@@ -2,15 +2,12 @@
 
 var settingsParser = require('../index');
 
-var files   = ['./test/_file1.scss', './test/_file2.scss', './test/_file3.scss'];
-var options = {
+settingsParser('./test/*.scss', {
   title: "Test Settings",
-  settingsPath: './test/_settings.scss',
+  output: './test/_settings.scss',
   groups: {
     one: 'Component One',
     two: 'Component Two',
     three: 'Component Three'
   }
-};
-
-settingsParser(files, options);
+});
