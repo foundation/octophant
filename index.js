77bf0d6354ac117e2f9584690d8b8e0a07decb45
Geoff Kimball <geoff@zurb.com>
2015-08-20 22:47:13 -0700

    Separate variable formatting into its own function

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 var buildContents  = require('./lib/buildContents');
+var buildSection   = require('./lib/buildSection');
 var extend         = require('util')._extend;
-var format         = require('util').format;
 var fs             = require('fs');
 var path           = require('path');
 var processSassDoc = require('./lib/processSassDoc');
-var repeatChar     = require('./lib/repeatChar');
 var sassdoc        = require('sassdoc');
 
 module.exports = function(paths, options) {
-  // The plugin must be called with a string or array of globs
-  if (typeof paths !== 'object') {
-    throw new Error('You need to specify paths for the plugin to parse.');
-  }
-
   options = extend({
     title:  'Settings',
     output: '_settings.scss',
     groups: {}
   }, options);
 
-  sassdoc.parse(paths, { verbose: false }).then(parse);
+  sassdoc.parse(paths).then(parse);
 
   function parse(data) {
     var outputPath = path.join(process.cwd(), options.output);
@@ -38,28 +32,8 @@ module.exports = function(paths, options) {
 
     // Iterate through each component
     for (var i in data) {
-      var group = data[i];
-      var title = options.groups[i] || i;
-      outputStream.write(format('// %s\n// %s\n\n', title, repeatChar('-', title.length)));
-
-      // Iterate through each variable within the component
-      for (var j in group) {
-        var variable = data[i][j];
-        var value = variable.context.value.split('\n');
-
-        if (value.length === 1) {
-          outputStream.write(format('// $%s: %s;\n', variable.context.name, variable.context.value));
-        }
-        else {
-          value = value.map(function(v, i) {
-            if (i === 0) return v;
-            return '// ' + v;
-          });
-          outputStream.write(format('// $%s: %s;\n', variable.context.name, value.join('\n')));
-        }
-      }
-
-      outputStream.write('\n');
+      var name = options.groups[i] || i;
+      outputStream.write(buildSection(name, data[i]));
     }
   }
 }
