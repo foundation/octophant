4a767aa238da4936e81182804efacc4b7a217c25
Geoff Kimball <geoff@zurb.com>
2015-08-20 22:22:11 -0700

    Separate table of contents generation into a function, to start making the library more testable

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
+var buildContents  = require('./lib/buildContents');
 var extend         = require('util')._extend;
 var format         = require('util').format;
 var fs             = require('fs');
-var repeatChar     = require('./lib/repeatChar');
-var multiline      = require('multiline');
 var path           = require('path');
 var processSassDoc = require('./lib/processSassDoc');
+var repeatChar     = require('./lib/repeatChar');
 var sassdoc        = require('sassdoc');
-
-var titleText = multiline(function() {/*
-//  %s
-//  %s
-//
-//  Table of Contents:
-//
-*/});
 
 module.exports = function(paths, options) {
   // The plugin must be called with a string or array of globs
@@ -27,35 +19,22 @@ module.exports = function(paths, options) {
     groups: {}
   }, options);
 
-  sassdoc.parse(paths, { verbose: true }).then(parse);
+  sassdoc.parse(paths, { verbose: false }).then(parse);
 
   function parse(data) {
     var outputPath = path.join(process.cwd(), options.output);
     data = processSassDoc(data);
 
-    // Erase the existing files
+    // Erase the existing file if necessary
     if (fs.existsSync(outputPath)) {
       fs.unlinkSync(outputPath);
     }
 
     var outputStream = fs.createWriteStream(outputPath, {flags: 'w'});
 
-    // Format title text with custom title
-    titleText = format(titleText, options.title, repeatChar('-', options.title.length));
-
     // Generate the table of contents
-    var n = 1;
-    for (var i in data) {
-      // Number formatted with leading zero
-      var c = n < 10 ? ' '+n : n;
-      // Component name
-      var t = options.groups[i] || i;
-      titleText += format('\n//  %s. %s', c, t);
-      n++;
-    }
-
+    var titleText = buildContents(options.title, Object.keys(data), options.groups);
     outputStream.write(titleText);
-    outputStream.write('\n\n');
 
     // Iterate through each component
     for (var i in data) {
--- test/test.js
+++ test/test.js
@@ -2,13 +2,14 @@
 
 var settingsParser = require('../index');
 
-var files   = ['./test/_file1.scss', './test/_file2.scss'];
+var files   = ['./test/_file1.scss', './test/_file2.scss', './test/_file3.scss'];
 var options = {
   title: "Test Settings",
   settingsPath: './test/_settings.scss',
   groups: {
     one: 'Component One',
-    two: 'Component Two'
+    two: 'Component Two',
+    three: 'Component Three'
   }
 };
 
