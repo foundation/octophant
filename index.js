119f0fc027eeffc060b6ea1c754e19c21b10d13c
Geoff Kimball <geoff@zurb.com>
2016-03-30 13:07:11 -0700

    Comment all the things

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ var path           = require('path');
 var processSassDoc = require('./lib/processSassDoc');
 var sassdoc        = require('sassdoc');
 
+/**
+ * Produces a Sass settings file from a set of Sass files, and writes it to disk.
+ * @param {string|string[]} files - Files to parse.
+ * @param {object} options - Output configuration.
+ * @param {string} options.title - Title of the settings file.
+ * @param {string} options.output - File path to write the file to.
+ * @param {object} options.groups - Custom names for groups.
+ * @param {string[]} options.imports - Sass files to load with `@import`.
+ * @param {string[]} options.sort - Custom sort order for component sections.
+ * @param {boolean} options._foundationShim - Adds the `@include add-foundation-colors` shim for Foundation for Sites 6.2.
+ * @param {function} cb - Function to run when the settings file has been written to disk.
+ */
 module.exports = function(files, options, cb) {
   options = extend({
     title:  'Settings',
@@ -32,6 +44,7 @@ module.exports = function(files, options, cb) {
       fs.unlinkSync(outputPath);
     }
 
+    // Create a stream to write the settings file to
     var outputStream = fs.createWriteStream(outputPath, {flags: 'w'});
 
     // Generate the table of contents
@@ -42,7 +55,7 @@ module.exports = function(files, options, cb) {
     var importText = buildImports(options.imports);
     outputStream.write(importText);
 
-    // Iterate through each component
+    // Generate each component section
     var n = 1;
     for (var i in data) {
       outputStream.write(buildSection(i, n, data[i], options._foundationShim));
